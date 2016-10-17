8e7ed3e954821c6b4f0520c7b1732602f426a311
Aeon Firebrand <lichking21st@windowslive.com>
2016-10-17 20:40:41 +1300

    Add project files.

---
+++ src/Ultimates-Cricket/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ultimates_Cricket.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
